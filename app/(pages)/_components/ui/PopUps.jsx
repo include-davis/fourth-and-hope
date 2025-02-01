ff6ce135225dda6cc20dcd64b138513d8253f214
Haylie Tan <haylie.tan.code@gmail.com>
2025-01-31 21:22:08 -0800

    Added Pop-Ups under ui under _components

---
+++ app/(pages)/_components/ui/PopUps.jsx
@@ -0,0 +1,13 @@
+import styles from "./PopUps.module.scss";
+
+// this is just example code but change as much as needed (feel free to completely scrap this!)
+
+export default function PopUp({ isOpen, onClose }) {
+  if (!isOpen) return null; 
+
+  return (
+    <div>
+      <button onClick={onClose}>EXIT</button>
+    </div>
+  );
+}
