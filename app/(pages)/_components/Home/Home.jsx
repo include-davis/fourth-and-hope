22e89f4b02edc6efab8369f9cda3fe6b04e4f423
Serina Moon <galagate.moon@gmail.com>
2025-02-05 00:03:02 -0800

    added div and components for homepage to work on added Home folder to public images to hold images for homepage

--- app/(pages)/_components/Home/Home.jsx
+++ app/(pages)/_components/Home/Home.jsx
@@ -1,9 +1,29 @@
 import styles from "./Home.module.scss";
+import Image from "next/image";
 
 export default function Home() {
     return (
-      <div>
-        Home
+    <>
+      <div className={styles.top_image}>
+        <Image
+          src ="../../../public/images/Home/houseHeader.svg"
+          width={500}
+          height={500}
+          alt="Picture of house with sunset and clouds behind it"
+        />
       </div>
+      <div className={styles.three_buttons}>
+        Buttons
+      </div>
+      <div className={styles.our_mission}>
+        Our Mission
+      </div>
+      <div className={styles.programs}>
+        Programs
+      </div>
+      <div className={styles.our_stories}>
+        Our Stories
+      </div>
+    </>
     );
 }
---
+++ app/(pages)/popups/page.jsx
@@ -0,0 +1,10 @@
+import PopUp from "../_components/ui/PopUps";
+
+export default function popups() {
+    return (
+      <main>
+        <div><PopUp/></div>
+      </main>
+    );
+  }
+  
