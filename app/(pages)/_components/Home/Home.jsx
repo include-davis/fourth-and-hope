b0d1cf0910389b5780cfe63e947968189aadc03b
Serina Moon <galagate.moon@gmail.com>
2025-02-05 14:02:42 -0800

    Added text for Our Mission section and created a separate div for the images under Our Mission

--- app/(pages)/_components/Home/Home.jsx
+++ app/(pages)/_components/Home/Home.jsx
@@ -10,8 +10,8 @@ import React, { useEffect, useState } from "react";
 const ImageSlider = () => {
   const images = [
     "/images/Home/houseHeader.svg",
-    "/images/Home/check_image.svg",
-    "/images/Home/kitchen_image.svg",
+    "/images/Home/checkHeader.svg",
+    "/images/Home/kitchenHeader.svg",
   ];
 
   const [currentImage, setCurrentImage] = useState(0);
@@ -21,22 +21,23 @@ const ImageSlider = () => {
     setCurrentImage((prev) => (prev + 1) % images.length);
   };
 
-  //automatically change image every 3 seconds
-  //FIXME: ask about how long each image should be up for and what the transition should look like
+  // automatically change image every 3 seconds
+  // FIXME: ask about how long each image should be up for and what the transition should look like
   useEffect(() => {
     const interval = setInterval(nextImage, 3000);
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className={styles.slider}>
-      <Image 
-      className={styles.sliderImage}
-      src={images[currentImage]}
-      width={500}
-      height={500}
-      alt="Slider"
-      />
+    <div className={styles.slider}> 
+        <Image 
+        className={styles.sliderImage}
+        src={images[currentImage]}
+        width={500}
+        height={500}
+        alt="Slider"
+        // style={{ opacity: index === currentImage ? 1 : 0}}
+        />
     </div>
   );
 };
@@ -53,7 +54,15 @@ export default function Home() {
         Buttons
       </div>
       <div className={styles.ourMission}>
-        Our Mission
+        <h1>Our Mission</h1>
+        <p>
+          Fourth & Hope is a community - a faith-based organization dedicated to providing care, shelter, and recovery to homeless populations in Yolo County. <br /><br />
+          We welcome all a provide a safe environment; we do not discriminate on the basis of religion.<br /><br />
+          Our values are compassion, faith, integrity, service, and stewardship. As Jesus Christ has served us, we serve His people.
+        </p>
+        <div className={styles.ourMissionImages}>
+
+        </div>
       </div>
       <div className={styles.programs}>
         Programs
