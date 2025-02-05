ded2519b15579658ca4dff28f91e22d01a44d727
Serina Moon <galagate.moon@gmail.com>
2025-02-05 13:26:58 -0800

    Created the ImageSlider function for image transitions at the top of the Homepage.

--- app/(pages)/_components/Home/Home.jsx
+++ app/(pages)/_components/Home/Home.jsx
@@ -1,16 +1,53 @@
+
+'use client'
+
 import styles from "./Home.module.scss";
 import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+
+/* function for header image slider */
+const ImageSlider = () => {
+  const images = [
+    "/images/Home/houseHeader.svg",
+    "/images/Home/check_image.svg",
+    "/images/Home/kitchen_image.svg",
+  ];
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  //function to move to the next image
+  const nextImage = () => {
+    setCurrentImage((prev) => (prev + 1) % images.length);
+  };
+
+  //automatically change image every 3 seconds
+  //FIXME: ask about how long each image should be up for and now images should change
+  useEffect(() => {
+    const interval = setInterval(nextImage, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className={styles.slider}>
+      <Image 
+      className={styles.sliderImage}
+      src={images[currentImage]}
+      width={500}
+      height={500}
+      alt="Slider"
+      />
+    </div>
+  );
+};
+
+
 
 export default function Home() {
     return (
     <>
-      <div className={styles.top_image}>
-        <Image
-          src ="../../../public/images/Home/houseHeader.svg"
-          width={500}
-          height={500}
-          alt="Picture of house with sunset and clouds behind it"
-        />
+      <div className={styles.image_slides}>
+        <ImageSlider/>
       </div>
       <div className={styles.three_buttons}>
         Buttons
