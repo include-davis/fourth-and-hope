8eba5acca7fad2e912b61270f7af2559b7eaff3e
michelleyeoh <michellew.yeoh@gmail.com>
2025-02-11 14:05:28 -0800

    comments

--- app/(pages)/_components/Json/personCard.jsx
+++ app/(pages)/_components/Json/personCard.jsx
@@ -1,4 +1,7 @@
- export default function PersonCard({name, title}){
+ //import any css files you use
+ 
+ export default function PersonCard({name, title}){ //takes in the props name & title
+    //this reuseable component should look really simple/condense
     return(
         <div>
             <h1>{name}</h1>
