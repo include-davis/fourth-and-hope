09c2f16216d4ab9ee2a9e46770f246e8cabc7e7e
hadiafifah <123785665+hadiafifah@users.noreply.github.com>
2025-02-11 12:42:27 -0800

    began header component

--- app/(pages)/_components/Header/Header.jsx
+++ app/(pages)/_components/Header/Header.jsx
@@ -2,8 +2,19 @@ import styles from "./Header.module.scss";
 
 export default function Header() {
     return (
-      <div>
-        Header
+      <div classnName="headerContainer">
+        <div className="contentContainer">Header</div>
+          <div className="logo"></div>
+          <div className="buttonsContainer">
+            <div className="otherbuttons"></div>
+              <div>Home</div>
+              <div>About</div>
+              <div>Get Involved</div>
+              <div>Events</div>
+              <div>Programs</div>
+              <div>Contact</div>
+            <div className="donateButton">Donate</div>
+          </div>
       </div>
     );
-}
+} // Questions: Images? Proper Font Sizing? 
