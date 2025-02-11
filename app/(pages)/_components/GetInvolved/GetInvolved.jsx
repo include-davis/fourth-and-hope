77e786318bfcef7c8e9d46caea99c861fa59408b
hmalhi-ucdavis <harinderjitsm@gmail.com>
2025-02-11 03:53:12 -0800

    Week 1 commit

--- app/(pages)/_components/GetInvolved/GetInvolved.jsx
+++ app/(pages)/_components/GetInvolved/GetInvolved.jsx
@@ -1,9 +1,24 @@
 import styles from "./GetInvolved.module.scss";
+import Image from "next/image";
+
+import Content from "../GetInvolved/Content/Content.jsx";
 
 export default function GetInvolved() {
     return (
-      <div>
-        Get Involved
+      <div className={styles.container}>
+        <Image
+          src="images/GetInvolved_images/get-involved-image.png"
+          alt="Description"
+          width={500}
+          height={300}
+          
+          unoptimized={true}
+          style={{width: "100%",height: "auto", marginBottom: "1.1%"}}
+        />
+        <Content/>
+
       </div>
+     
+        
     );
 }
