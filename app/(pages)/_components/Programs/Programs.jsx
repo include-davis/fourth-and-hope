de2be0ef7407b5f36a2d6d880bbc81538149262c
JarredPeros <jarredperos@gmail.com>
2025-02-10 20:44:57 -0800

    ~ Notes ~
    Notes on what to complete

--- app/(pages)/_components/Programs/Programs.jsx
+++ app/(pages)/_components/Programs/Programs.jsx
@@ -1,9 +1,25 @@
 import styles from "./Programs.module.scss";
 
+//import Image from "next/image";
+//import Link from "next/link"
+
 export default function Programs() {
     return (
       <div>
-        Programs
+        Programs1
       </div>
     );
 }
+
+
+/*
+
+Notes
+- Divide into thirds 
+  - Animated Numbers
+  - 
+- USE PERCENTAGES - No pixels
+- Numbers increase from 0 
+- Components Folder
+
+*/
