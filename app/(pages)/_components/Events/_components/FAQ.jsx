bc86e092f5aa383416eb619a084269294fcec002
Holly Zheng <hzlly.zheng@gmail.com>
2025-02-11 11:36:18 -0800

    recap

---
+++ app/(pages)/_components/Events/_components/FAQ.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, {useState} from "react";
+import "./FAQ.scss";
+
+const FAQ = ({question, answer}) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    // Split the answer into an array of paragraphs using the line breaks (\n)
+    const answerParagraphs = answer.split("\n").map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
+     ));
+
+    return (
+        <div className="faq">
+            <div className="faq-question" onClick={() => setIsOpen(!isOpen)}>
+                <span>{question}</span>
+                <button className="toggle-btn">{isOpen ? "-" : "+"}</button>
+            </div>
+    
+            {isOpen && (
+                <div className="faq-answer">
+                    <div>{answerParagraphs}</div>
+                </div>
+            )}
+        </div>
+    );    
+};
+
+export default FAQ;
+
+
+
+    
