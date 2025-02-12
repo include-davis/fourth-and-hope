3318c3ef2f2e4cc9ffd2e2c70f3f42fcb3568a52
Haylie Tan <haylie.tan.code@gmail.com>
2025-02-11 17:09:42 -0800

    Update page.jsx

--- app/(pages)/page.jsx
+++ app/(pages)/page.jsx
@@ -1,12 +1,12 @@
 import Home from './_components/Home/Home';
-import PrimaryButton from './_components/Button/PrimaryButton';
-import SecondaryButton from './_components/Button/SecondaryButton';
+// import PrimaryButton from './_components/Button/PrimaryButton';
+// import SecondaryButton from './_components/Button/SecondaryButton';
 
 export default function Homepage() {
   return (
     <main>
-      <PrimaryButton name="Primary Button" link="https://example.com/donate" />
-      <SecondaryButton name="Secondary Button" link="https://example.com/donate" />
+       {/* <PrimaryButton name="Primary Button" link="https://example.com/donate" />
+       <SecondaryButton name="Secondary Button" link="https://example.com/donate" /> */}
       <Home/>
     </main>
   );
