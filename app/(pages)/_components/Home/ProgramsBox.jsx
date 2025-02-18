ba960871b920cb206cbeb7b542b3ecfeb21ca3d5
Serina Moon <galagate.moon@gmail.com>
2025-02-18 12:10:17 -0800

    organized programs section

--- app/(pages)/_components/Home/ProgramsBox.jsx
+++ app/(pages)/_components/Home/ProgramsBox.jsx
@@ -10,14 +10,22 @@ export default function ProgramsBox({imageSrc, altText, Title, bodyText, href}){
                 <Image
                 src={imageSrc}
                 alt={altText}
-                width={500}
-                height={500}  
+                width={335}
+                height={335}  
                 />
-                <h1>{Title}</h1>
+                <h2>{Title}</h2>
                 <p>{bodyText}</p>
             </div>
             <Link href={href} className={styles.learnMore}>
                 learn more
+                {/* 
+                    how do you want the learn more button??
+                    line and arrow when hover
+                    always line
+                    always arrow 
+                    always line and arrow
+                    color change on hover? bold?
+                */}
             </Link>
         </div>
 
