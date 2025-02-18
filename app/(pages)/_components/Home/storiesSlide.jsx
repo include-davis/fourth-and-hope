adba09e79dfcb28bd6e030615dcb04f0ede576b5
Serina Moon <galagate.moon@gmail.com>
2025-02-18 15:00:56 -0800

    Started on the html for the our stories component.

--- app/(pages)/_components/Home/storiesSlide.jsx
+++ app/(pages)/_components/Home/storiesSlide.jsx
@@ -1,3 +1,22 @@
 import styles from './storiesSlide.module.scss'
 import React from "react";
 import Image from "next/image";
+
+export default function storiesSlide({heading, bodyText, imageSrc, altText}){
+    
+    return(
+        <>        
+        <div className={styles.storiesText}>
+            <h2>{heading}</h2>
+            <p>{bodyText}</p>
+        </div>
+        <Image
+            className={styles.storiesImage}
+            src={imageSrc}
+            alt= {altText}
+            width={500}
+            height={500}
+        />
+        </>
+    );
+}
