69448acfd86bf6b822b8b9ea6a7d4d1650556119
Jae234 <jjtimemoon10@gmail.com>
2025-02-17 17:12:15 -0800

    Backend
    Added backend for Contact, need API and other stuff, will be added later.

--- app/(pages)/_components/Contact/Contact.jsx
+++ app/(pages)/_components/Contact/Contact.jsx
@@ -1,9 +1,52 @@
 import styles from "./Contact.module.scss";
 
+
 export default function Contact() {
     return (
       <div>
-        Contact
+        <div>
+          <img src="/contact_picture.svg" alt="" />
+        </div>
+        <div className={styles.header}>
+          <h1> Contact </h1>
+        </div>
+
+        <div className ={styles.infocontainer}>
+          <div className={styles.item}>
+            
+            <p></p>
+          </div>
+
+          <div className ={styles.item}>
+
+          </div>
+
+          <div className = {styles.item}>
+
+          </div>
+        </div>
+
+        <div className = {styles.feedback}>
+          <h3>Additional Feedback</h3>
+          <p>At Fourth & Hope, we believe in hearing from you and all of our stakeholders. Your ideas and solutions are valuable to our continued success.</p>
+          <p>Replies will come from <a href="mailto:jjtimemoon10@gmail.com">support@fourthandhope.org</a></p>
+          
+          
+          <form id="contactForm" action="http://localhost:3000/contact" method="POST">
+            <div className = {styles.formgrid}>
+              <input type="text" name="fName" placeholder="First Name"/>
+              <input type="text" name="lName" placeholder="Last Name" />
+              <input type="email" name="email" placeholder="Email*" required/>
+              <input type="text" name="company" placeholder="Company" />
+              <textarea placeholder="Please add details about your inquiry"></textarea>
+            </div>
+            <button type = "submit">Submit</button>
+          </form>
+          
+          
+        </div>
+        
+
       </div>
     );
 }
