a68a1d21f650767fde386125a1fc22c5a776568b
hadiafifah <123785665+hadiafifah@users.noreply.github.com>
2025-03-05 03:20:28 -0800

    updated mobile view based on final figma

--- app/(pages)/_components/Header/Header.jsx
+++ app/(pages)/_components/Header/Header.jsx
@@ -5,14 +5,20 @@ import {useState} from "react";
 
 export default function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () =>{
+      setMenuOpen((prev) => !prev);
+    }
     return (
-      <div className={styles.headerContainer}>
+      <>
+      <div className={`headerContainer ${menuOpen ? styles.open : ""}`}>
         <div className={styles.contentContainer}>
           <div className={styles.logo}><img src="/images/headerlogo.svg" alt="Fourth Hope Logo" className={styles.logo}/></div>
-
-          {/* Desktop Navigation */}
+          <div className={styles.xHolder}><button className={styles.xButton} onClick={toggleMenu}>
+            <img src="/images/exitsymbol.svg" alt="Header Exit Button"/>
+          </button></div>
           <div className={styles.buttonsContainer}>
-            <a href="/donate" className={styles.button}>Donate</a>
+            <a href="/donate" className={[styles.button, styles.bold].join(' ')}>Donate</a>
             <div className={styles.otherbuttons}>
               <Link href="/" className={[styles.links, styles.bold].join(' ')}>Home</Link>
               <Link href="/about" className={styles.links}>About</Link>
@@ -22,22 +28,14 @@ export default function Header() {
               <Link href="/contact" className={styles.links}>Contact</Link>
             </div>
           </div>
-          {/* Desktop Navigation */}
-
-          {/* Mobile Navigation */}
-          <button className={styles.menuButton} onClick={() => setMenuOpen(!menuOpen)}>
+          <button className={styles.menuButton} onClick={toggleMenu}>
             <img src="/images/menu-rounded.svg" alt="Header Dropdown Button"/>
           </button>
+
+      
         </div>
-        <div className={`${styles.dropdownMenu} ${menuOpen ? styles.open : ""}`}>
-            <Link href="/" className={styles.links}>Home</Link>
-            <Link href="/about" className={styles.links}>About</Link>
-            <Link href="/get-involved" className={styles.links}>Get Involved</Link>
-            <Link href="/events" className={styles.links}>Events</Link>
-            <Link href="/programs" className={styles.links}>Programs</Link>
-            <Link href="/contact" className={styles.links}>Contact</Link>
-        </div>
-          {/* Mobile Navigation */}
       </div>
+      {menuOpen && <div className={styles.overlay} onClick={toggleMenu}></div>} 
+      </>
     );
 } // Questions: Images? Proper Font Sizing? 
