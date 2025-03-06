cd9a87ca937f93148a2b155162f1bb8b38c0821f
Rachie Yim <rachieyim@gmail.com>
2025-03-06 01:38:28 -0800

    changes

--- app/(pages)/_components/About/StaffSection.jsx
+++ app/(pages)/_components/About/StaffSection.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+
 import MemberCard from './MemberCard';
 import boardData from './boardData.json';
 const { executiveStaff } = boardData;
@@ -7,13 +8,12 @@ import styles from './About.module.scss';
 export default function StaffSection() {
   return (
     <section className={styles.staffSection}>
-      {/* Add the executiveStaffHeading class to the h2 */}
-      <h2 className={styles.executiveStaffHeading}>Executive Staff</h2>
+      <h2>Executive Staff</h2>
       
       <div className={styles.staffGrid}>
         {executiveStaff.map((member, index) => (
           <MemberCard 
-            key={member.name || index}
+            key={index}
             name={member.name}
             title={member.title}
             email={member.email}
