d57b1b8a122cb269b5782fd3afdfa6a3b2eea796
Jae234 <jjtimemoon10@gmail.com>
2025-03-09 01:18:54 -0800

    everything done, but I have to fix a layout issue

--- app/(pages)/_components/Contact/Contact.jsx
+++ app/(pages)/_components/Contact/Contact.jsx
@@ -1,154 +1,32 @@
 import styles from "./Contact.module.scss";
 import Image from "next/image";
+import ContactCard from "./ContactCard";
 
 
 export default function Contact() {
     return (
       <div>
         <div>
-          
-          <img src="./images/Images_Contact/Contactpic.svg" alt="Contact Image" style={{ width: '100%', height: '20%' }}/>
-        </div>
-        <div className={styles.header}>
-          <h1> CONTACT </h1>
-        </div>
-
-        <div className ={styles.infocontainer}>
-        
-          <div className={styles.shelter}>
-            <img src="./images/Images_Contact/shelter.svg" alt="Shelter" />
-            <h4>Emergency Shelter Services</h4>
-            <div className={styles.section}>
-              <div className={styles.icon}>
-                
-                <img src="./images/Images_Contact/iconamoon_location-pin-bold.svg" alt="Location" style={{ width: '32px', height: '32px' }}/>
-                
-              </div>
-              <div className={styles.shortinfo}>
-                <p>1901 East Beamer St, Woodland, CA, 95776</p>
-              </div>
-              
-            </div>
-
-            <div className={styles.section}>
-
-            <div className={styles.upicon}>
-                <img src="./images/Images_Contact/solar_phone-linear.svg" alt="Phone" style={{ width: '32px', height: '32px' }}/>
-              
-              </div>
-
-              <div className={styles.shortinfo}>
-                <p>530-661-1218</p>
-
-              </div>
-
-            </div>
-
-            <div className={styles.section}>
-
-              <div className={styles.icon}>
-                <img src="./images/Images_Contact/line-md_email.svg" alt="Email" style={{ width: '32px', height: '32px' }}/>
-
-              </div>
-
-              <div className={styles.shortinfo}>
-                <a href="mailto:shelter@fourthandhope.org">shelter@fourthandhope.org </a>
-                <a href="mailto:volunteer@fourthandhope.org">volunteer@fourthandhope.org</a>
-
-              </div>
-
-
-            </div>
+          <div className = {styles.contactImage}>
+            <Image
+              src="/images/Images_Contact/Contactpic.svg"
+              alt="Contact Image"
+              width={0}
+              height={0}
+              style={{width: "100%", height:"auto"}}
+            />
           </div>
 
-          <div className ={styles.treatment}>
-            <img src="./images/Images_Contact/treatment.svg" alt="Treatment" />
-            <h4>Treatment at Walters House</h4>
 
-            <div className={styles.section}>
-              <div className={styles.icon}>
-                
-                <img src="./images/Images_Contact/iconamoon_location-pin-bold.svg" alt="Location" style={{ width: '32px', height: '32px' }}/>
-                
-              </div>
-              <div className={styles.shortinfo}>
-                <p>285 Fourth St. Woodland, CA, 95695</p>
-              </div>
-              
-            </div>
-
-            <div className={styles.section}>
-
-            <div className={styles.upicon}>
-                <img src="./images/Images_Contact/solar_phone-linear.svg" alt="Phone" style={{ width: '32px', height: '32px' }}/>
-              
-              </div>
-
-              <div className={styles.shortinfo}>
-                <p>530-662-2699</p>
-
-              </div>
-
-            </div>
-
-            <div className={styles.section}>
-
-              <div className={styles.icon}>
-                <img src="./images/Images_Contact/line-md_email.svg" alt="Email" style={{ width: '32px', height: '32px' }}/>
-
-              </div>
-
-              <div className={styles.shortinfo}>
-                <a href="mailto:recovery@fourthandhope.org">recovery@fourthandhope.org</a>
-
-              </div>
-            </div>
+          <div className={styles.Title}>
+            <h1> CONTACT </h1>
           </div>
 
-            
-            
-            
-
-        
-
-          <div className = {styles.inquiries}>
-            <img src="./images/Images_Contact/others.svg" alt="Others" />
-            <h4>All Other Inquiries</h4>
-            <div className={styles.shortsection}>
-              <p>Such as housing, fundraising, and other services</p>
-            </div>
-            
-
-            <div className = {styles.section}>
-              <div className={styles.upicon}>
-                <img src="./images/Images_Contact/solar_phone-linear.svg" alt="Phone" style={{ width: '32px', height: '32px' }}/>
-                
-              </div>
-
-              <div className={styles.shortinfo}>
-                <p>530-661-1218</p>
-
-              </div>
-
-            </div>
-
-            <div className = {styles.section}>
-              <div className={styles.icon}>
-                <img src="./images/Images_Contact/line-md_email.svg" alt="Email" style={{ width: '32px', height: '32px' }}/>
-
-              </div>
-
-              <div className={styles.shortinfo}>
-                <a href="mailto:support@fourthandhope.org">support@fourthandhope.org</a>
-
-              </div>
-
-            </div>
+          {/* Contact Elements */}
+          <div className={styles.contactcards}>
+            <ContactCard/>
           </div>
-
         </div>
-
-
 
 
 
@@ -161,7 +39,7 @@ export default function Contact() {
             <p>Replies will come from <a href="mailto:support@fourthandhope.org">support@fourthandhope.org</a></p>
           </div>
           
-          
+          {/*Feedback Form*/}
           <form className={styles.contactform} id="contactForm" action="http://localhost:3000/contact" method="POST">
             <div className = {styles.inputgroup}>
               <input type="text" name="fName" placeholder="First Name" id ="fName" />
@@ -171,8 +49,8 @@ export default function Contact() {
               <input type="email" name="email" placeholder="Email*" required/>
               <input type="text" name="company" placeholder="Company" />
             </div>
-            <textarea placeholder="Please add details about your inquiry"></textarea>
-            <button type = "submit">Submit</button>
+            <textarea className = {styles.textarea} placeholder="Please add details about your inquiry"></textarea>
+            <button className = {styles.button} type = "submit">Submit</button>
           </form>
           
         </div>
