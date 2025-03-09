9f70b0faa0e332a1c88f5cb50651871c268c4d20
Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>
2025-03-08 18:54:41 -0800

    added try catch

--- app/(pages)/page.jsx
+++ app/(pages)/page.jsx
@@ -5,47 +5,55 @@ import heroGalleryFallbackData from './_data/hero-gallery.json';
 // import SecondaryButton from './_components/Button/SecondaryButton';
 
 async function getImages(){
-  const res = await fetch(`${process.env.CMS_BASE_URL}/api/content/hero-gallery?_published=true`, 
-    { next: 
-      {
-        tags: "cms"
+  try {
+    const res = await fetch(`${process.env.CMS_BASE_URL}/api/content/hero-gallery?_published=true`, 
+      { next: 
+        {
+          tags: "cms"
+        }
       }
+    );
+    const data = await res.json();
+    if(!data.ok || data.body.length === 0){
+      throw new Error();
     }
-  );
-  const data = await res.json();
-  if(!data.ok || data.body.length === 0){
+
+    const images = [];
+    data.body.forEach(contentItem => {
+      contentItem.main_image.forEach(imageItem => {
+        images.push(imageItem.src);
+      });
+    });
+
+    return images;
+  } catch {
     console.log('Failed to fetch hero gallery images');
     return heroGalleryFallbackData;
   }
-
-  const images = [];
-  data.body.forEach(contentItem => {
-    contentItem.main_image.forEach(imageItem => {
-      images.push(imageItem.src);
-    });
-  });
-
-  return images;
 }
 
 async function getStories(){
-  const res = await fetch(`${process.env.CMS_BASE_URL}/api/content/story-cards?_published=true`, 
-    { next: 
-      {
-        tags: "cms"
+  try {
+    const res = await fetch(`${process.env.CMS_BASE_URL}/api/content/story-cards?_published=true`, 
+      { next: 
+        {
+          tags: "cms"
+        }
       }
+    );
+    const data = await res.json();
+    if(!data.ok || data.body.length === 0){
+      throw new Error();
     }
-  );
-  const data = await res.json();
-  if(!data.ok || data.body.length === 0){
+    
+    //TODO: add alt text to cms schema 
+    const parsedData = data.body.map((storyItem) => ({imageSrc: storyItem.main_image[0].src, altText: storyItem.image_alt_text, heading: storyItem.title, bodyText: storyItem.description}));
+  
+    return parsedData;
+  } catch {
     console.log('Failed to fetch story cards');
     return storyCardsFallbackData;
   }
-
-  //TODO: add alt text to cms schema 
-  const parsedData = data.body.map((storyItem) => ({imageSrc: storyItem.main_image[0].src, altText: storyItem.image_alt_text, heading: storyItem.title, bodyText: storyItem.description}));
-
-  return parsedData;
 }
 
 
