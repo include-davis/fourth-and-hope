fd1bad5753c89b73f3546c74c9ab161dbcf2ae60
Haylie Tan <haylie.tan.code@gmail.com>
2025-03-08 17:51:57 -0800

    CMS for Staff

--- app/(pages)/_components/About/StaffSection.jsx
+++ app/(pages)/_components/About/StaffSection.jsx
@@ -1,11 +1,32 @@
-'use client';
+// 'use client';
 
 import MemberCard from './MemberCard';
-import boardData from './boardData.json';
-const { executiveStaff } = boardData;
+// import boardData from './boardData.json';
+// const { executiveStaff } = boardData;
 import styles from './About.module.scss';
 
-export default function StaffSection() {
+async function getPeople() {
+  const res = await fetch(`${process.env.CMS_BASE_URL}/api/content/people`, {
+    next: { tags: "cms" }
+  });
+
+  const data = await res.json(); // ok, body, error
+
+  if(!data.ok){
+    throw new Error('Failed to fetch people');
+  }
+
+  return data.body.map(person => ({
+    image: person.main_image,  
+    name: person.name,  
+    position: person.position,
+    type: person.type,
+  }));
+}
+
+export default async function StaffSection() {
+  const executiveStaff = await getPeople();
+
   return (
     <section className={styles.staffSection}>
       {/* Board Meeting Info button for mobile */}
