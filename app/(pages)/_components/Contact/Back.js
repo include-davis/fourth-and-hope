62c7265d866f2e4692ead04e1c264ce7e0f620a8
michelleyeoh <michellew.yeoh@gmail.com>
2025-03-09 13:36:03 -0700

    eslint

--- app/(pages)/_components/Contact/Back.js
+++ app/(pages)/_components/Contact/Back.js
@@ -1,28 +1,28 @@
-require('dotenv').config();  // To load environment variables from .env file
+require("dotenv").config(); // To load environment variables from .env file
 
-const express = require('express');
-const sgMail = require('@sendgrid/mail');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const sgMail = require("@sendgrid/mail");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
 // Initialize SendGrid with the API key
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);  // Get SendGrid API key from environment
+sgMail.setApiKey(process.env.SENDGRID_API_KEY); // Get SendGrid API key from environment
 
 // Middleware to parse JSON data from the frontend
 app.use(cors());
 app.use(bodyParser.json());
 
 // POST route for the contact form
-app.post('/contact', (req, res) => {
+app.post("/contact", (req, res) => {
   const { name, email, message } = req.body;
 
   // Create the email data in JSON format
   const emailData = {
-    to: 'support@fourthandhope.org',  // company's email
-    from: email,  // The user's email from the form
+    to: "support@fourthandhope.org", // company's email
+    from: email, // The user's email from the form
     subject: `Message from ${name} - Contact Form`,
     text: `Message: ${message}\n\nFrom: ${name}\nEmail: ${email}`,
   };
@@ -31,10 +31,14 @@ app.post('/contact', (req, res) => {
   sgMail
     .send(emailData)
     .then(() => {
-      res.status(200).json({ message: 'Your message has been sent successfully!' });
+      res
+        .status(200)
+        .json({ message: "Your message has been sent successfully!" });
     })
-    .catch(error => {
-      res.status(500).json({ message: 'Error sending email: ' + error.message });
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ message: "Error sending email: " + error.message });
     });
 });
 
@@ -42,4 +46,3 @@ app.post('/contact', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
