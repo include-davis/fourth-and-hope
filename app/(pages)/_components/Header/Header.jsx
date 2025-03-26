69bf6deeb35bc61176c62df08e2dc5e1346e9066
Haylie Tan <haylie.tan.code@gmail.com>
2025-03-25 21:52:50 -0700

    Finalized header fixes

--- app/(pages)/_components/Header/Header.jsx
+++ app/(pages)/_components/Header/Header.jsx
@@ -1,50 +1,70 @@
 "use client";
-import styles from "./Header.module.scss";
+import { useState } from "react";
 import Link from "next/link";
+import styles from "./Header.module.scss";
+import MobileMenu from "./MobileMenu"; // Ensure path is correct
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen((prev) => !prev);
+
   return (
-    <div className={styles.headerContainer}>
-      <div className={styles.contentContainer}>
-        <div className={styles.logo}>
-          <Link href="/" passHref>
-            <img
-              src="/images/headerlogo.svg"
-              alt="Fourth Hope Logo"
-              className={styles.logo}
-            />
-          </Link>
-        </div>
-        <div className={styles.buttonsContainer}>
-          <a href="/donate" className={[styles.button, styles.bold].join(" ")}>
-            Donate
-          </a>
-          <div className={styles.otherbuttons}>
-            <Link href="/" className={[styles.links, styles.bold].join(" ")}>
-              Home
-            </Link>
-            <Link href="/about" className={styles.links}>
-              About
-            </Link>
-            <Link href="/get-involved" className={styles.links}>
-              Get Involved
-            </Link>
-            <Link href="/events" className={styles.links}>
-              Events
-            </Link>
-            <Link href="/programs" className={styles.links}>
-              Programs
-            </Link>
-            <Link href="/contact" className={styles.links}>
-              Contact
+    <>
+      <header className={styles.headerContainer}>
+        <div className={styles.contentContainer}>
+          <div className={styles.logo}>
+            <Link href="/" passHref>
+              <img
+                src="/images/headerlogo.svg"
+                alt="Fourth Hope Logo"
+                className={styles.logo}
+              />
             </Link>
           </div>
+          <nav className={styles.buttonsContainer}>
+            <a href="/donate" className={`${styles.button} ${styles.bold}`}>
+              Donate
+            </a>
+            <div className={styles.otherbuttons}>
+              <Link href="/" className={`${styles.links} ${styles.bold}`}>
+                Home
+              </Link>
+              <Link href="/about" className={styles.links}>
+                About
+              </Link>
+              <Link href="/get-involved" className={styles.links}>
+                Get Involved
+              </Link>
+              <Link href="/events" className={styles.links}>
+                Events
+              </Link>
+              <Link href="/programs" className={styles.links}>
+                Programs
+              </Link>
+              <Link href="/contact" className={styles.links}>
+                Contact
+              </Link>
+            </div>
+          </nav>
+
+          {/* Hamburger icon (shown on mobile via CSS media queries) */}
+          <button className={styles.menuButton} onClick={toggleMenu}>
+            <img src="/images/menu-rounded.svg" alt="Hamburger icon" />
+          </button>
         </div>
-        {/* Hamburger icon button that appears on mobile due to SCSS media queries */}
-        <button className={styles.menuButton}>
-          <img src="/images/menu-rounded.svg" alt="Hamburger icon" />
-        </button>
-      </div>
-    </div>
+      </header>
+
+      {/* Render overlay + MobileMenu when open */}
+      {menuOpen && (
+        <>
+          {/* Overlay that darkens the background */}
+          <div className={styles.overlay} onClick={toggleMenu} />
+
+          {/* The actual slide-in mobile menu */}
+          <MobileMenu toggleMenu={toggleMenu} />
+        </>
+      )}
+    </>
   );
 }
--- app/(pages)/_components/Header/MobileMenu.jsx
+++ app/(pages)/_components/Header/MobileMenu.jsx
@@ -1,61 +1,41 @@
-// "use client";
-// import styles from "./Header.module.scss";
-// import Link from "next/link";
-// import { useState } from "react";
-// import MobileMenu from "./MobileMenu"; // Make sure the path is correct.
+import Link from "next/link";
+import styles from "./MobileMenu.module.scss";
 
-// export default function Header() {
-//   const [menuOpen, setMenuOpen] = useState(false);
+export default function MobileMenu({ toggleMenu }) {
+  return (
+    <div className={styles.mobileMenu}>
+      {/* Close (X) button in top-right */}
+      <button className={styles.closeButton} onClick={toggleMenu}>
+        &times;
+      </button>
 
-//   const toggleMenu = () => {
-//     setMenuOpen((prev) => !prev);
-//   };
+      <nav className={styles.nav}>
+        <Link href="/" onClick={toggleMenu}>
+          Home
+        </Link>
+        <Link href="/about" onClick={toggleMenu}>
+          About
+        </Link>
+        <Link href="/get-involved" onClick={toggleMenu}>
+          Get Involved
+        </Link>
+        <Link href="/events" onClick={toggleMenu}>
+          Events
+        </Link>
+        <Link href="/programs" onClick={toggleMenu}>
+          Programs
+        </Link>
+        <Link href="/contact" onClick={toggleMenu}>
+          Contact
+        </Link>
+      </nav>
 
-//   return (
-//     <>
-//       <div className={`headerContainer ${menuOpen ? styles.open : ""}`}>
-//         <div className={styles.contentContainer}>
-//           <div className={styles.logo}>
-//             <Link href="/" passHref>
-//               <img
-//                 src="/images/headerlogo.svg"
-//                 alt="Fourth Hope Logo"
-//                 className={styles.logo}
-//               />
-//             </Link>
-//           </div>
-//           <div className={styles.buttonsContainer}>
-//             <a href="/donate" className={[styles.button, styles.bold].join(" ")}>
-//               Donate
-//             </a>
-//             <div className={styles.otherbuttons}>
-//               <Link href="/" className={[styles.links, styles.bold].join(" ")}>
-//                 Home
-//               </Link>
-//               <Link href="/about" className={styles.links}>
-//                 About
-//               </Link>
-//               <Link href="/get-involved" className={styles.links}>
-//                 Get Involved
-//               </Link>
-//               <Link href="/events" className={styles.links}>
-//                 Events
-//               </Link>
-//               <Link href="/programs" className={styles.links}>
-//                 Programs
-//               </Link>
-//               <Link href="/contact" className={styles.links}>
-//                 Contact
-//               </Link>
-//             </div>
-//           </div>
-//           <button className={styles.menuButton} onClick={toggleMenu}>
-//             <img src="/images/menu-rounded.svg" alt="Header Dropdown Button" />
-//           </button>
-//         </div>
-//       </div>
-//       {/* Instead of rendering an overlay here, we render the MobileMenu component when menuOpen is true */}
-//       {menuOpen && <MobileMenu toggleMenu={toggleMenu} />}
-//     </>
-//   );
-// }
+      {/* Optional donate button at the bottom */}
+      <div className={styles.donateButtonWrapper}>
+        <a href="/donate" className={styles.donateButton} onClick={toggleMenu}>
+          Donate
+        </a>
+      </div>
+    </div>
+  );
+}
